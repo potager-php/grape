7843bd22ab624b7468cbfdbc27cb0e930d613ecd
Théo Vandormael <theo.vandormael@proton.me>
2025-08-24 02:41:43 +0200

    remove coverage from repo

--- src/Helpers/Url.php
+++ src/Helpers/Url.php
@@ -6,8 +6,6 @@ class Url
 {
     public static function validate(string $str): bool
     {
-        if (!is_string($str))
-            return false;
         return filter_var($str, FILTER_VALIDATE_URL);
     }
 }
